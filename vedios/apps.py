c3f852e1dec04c869ccd50522f4bd1a24d63e268
jod431047 <jod431047@gmail.com>
2023-09-17 12:22:59 +0200

    add classes in models

--- vedios/apps.py
+++ vedios/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PostsConfig(AppConfig):
+class VediosConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'posts'
+    name = 'vedios'
